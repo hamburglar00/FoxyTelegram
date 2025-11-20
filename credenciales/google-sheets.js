9f673eed9d8265c10002721c76811800257c604d
hamburglar00 <hamburglar002@proton.me>
2025-11-20 13:10:04 -0300

    Update google-sheets.js

--- credenciales/google-sheets.js
+++ credenciales/google-sheets.js
@@ -1,3 +1,3 @@
 export const CONFIG_SHEETS = {
-  GOOGLE_SHEETS_URL: ''
+  GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycbx0NJF--T686jeYT1uR_DlR_vQ8l7giIGR4WcbIZkQdCWOuq8nJYVp0jxU5CamtiW0f/exec'
 };
